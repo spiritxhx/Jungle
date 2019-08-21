70c6017d66c69711820dcd944b5f1f369f65a79c
spiritxhx <spiritxhx@gmail.com>
2019-08-21 18:20:29 -0400

    complete the adding category function and played a bit with bootstra

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -18,5 +18,11 @@ class Admin::CategoriesController < ApplicationController
     end
   end
 
+  private
 
+    def category_params
+      params.require(:category).permit(
+        :name
+      )
+    end
 end
