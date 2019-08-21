77616b367f384171208e10b42ae4eacfee3fce6a
spiritxhx <spiritxhx@gmail.com>
2019-08-21 12:11:01 -0400

    add a http basic auth and remember to restart the server

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name: ENV['STRIPE_ADMIN_USERNAME'], password: ENV['STRIPE_ADMIN_PASSWORD']
 
   def index
     @products = Product.order(id: :desc).all
