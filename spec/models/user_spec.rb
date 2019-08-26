1829747cb65a57e3c8087b4963887f67b2b79d64
spiritxhx <spiritxhx@gmail.com>
2019-08-26 17:00:10 -0400

    add password check and pass the test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe User, type: :model do
     subject {described_class.new(name: 'Bob', email: 'bob@me', password: 'qweasd123', password_confirmation: 'qweasd123')}
     describe 'Password exists' do 
       it 'is valid with a valid password' do
-        # subject.valid?
         expect(subject).to be_valid
       end
       it 'should have a password' do
@@ -17,13 +16,23 @@ RSpec.describe User, type: :model do
 
     describe 'Password_confirmation exists' do 
       it 'is valid with a valid password_confirmation' do
-        # subject.valid?
         expect(subject).to be_valid
       end
       it 'should have a password_confirmation' do
         subject.password_confirmation = nil
         subject.valid?
         expect(subject.errors[:password_confirmation].first).to eq"can't be blank"
+      end
+    end
+
+    describe 'Password matches' do
+      it 'is valid with the same password and confirmation' do
+        expect(subject).to be_valid
+      end
+      it 'is invalid without same password and confirmation' do
+        subject.password='123456'
+        subject.valid?
+        expect(subject.errors[:password_confirmation].first).to eq"doesn't match Password"
       end
     end
   end
