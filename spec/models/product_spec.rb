c831690cb55def0e99070aba93956b0be0cdb960
spiritxhx <spiritxhx@gmail.com>
2019-08-26 14:45:00 -0400

    add the first 2 tests for name validation

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,5 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
+    cat = Category.new(name:"meat")
+    subject {described_class.new(price_cents:100, name: 'Anything', quantity: 2, category: cat)}
+    describe 'Name validation' do 
+      it 'is valid with a valid name' do
+        subject.name = 'Anything'
+        expect(subject).to be_valid
+      end
+      it 'is not valid without a valid name' do
+        subject.name = nil
+        expect(subject).to_not be_valid
+      end
+    end
   end
 end
