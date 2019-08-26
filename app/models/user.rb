9974db8514c72c3e8866b2b529e2333d19b4d37b
spiritxhx <spiritxhx@gmail.com>
2019-08-26 19:42:37 -0400

    New authentication (class) method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,13 @@ class User < ActiveRecord::Base
   validates :email, {presence: true, uniqueness: {case_sensitive: false}}
   validates :name, presence: true
 
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email)
+    if user && user.authenticate(password)
+      return user
+    else 
+      return nil
+    end
+  end
+
 end
