1d5e5c0682466428876e31eb0ee551bc8c33ea56
spiritxhx <spiritxhx@gmail.com>
2019-08-27 16:08:39 -0400

    finish the mart test

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "AddToCarts", type: :feature, js: :true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "should add 1 item to shopping cart" do
+    visit root_path
+
+    click_button("Add", match: :first)
+
+    expect(page).to have_content('My Cart (1)')
+
+    save_screenshot
+
+  end
+
+end
